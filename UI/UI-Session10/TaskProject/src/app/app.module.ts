5e4835458ac547a75ccadc1a323bbba8275c179d
meta-vinod-sharma1 <vinod.sharma1@metacube.com>
2019-10-14 19:33:48 +0530

    Changes Updated

--- UI/UI-Session10/TaskProject/src/app/app.module.ts
+++ UI/UI-Session10/TaskProject/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskServiceService } from './task-service.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { TaskServiceService } from './task-service.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,ReactiveFormsModule
   ],
   providers: [TaskServiceService],
   bootstrap: [AppComponent]
